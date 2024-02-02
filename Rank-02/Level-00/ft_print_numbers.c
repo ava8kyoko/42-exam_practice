4b1e208acb9e8ee30112315d1807ee7dd678bc17
ava_kyoko <ava8kyoko@gmail.com>
2024-02-02 12:46:39 -0500

    Update ft_print_numbers.c

--- Rank-02/Level-00/ft_print_numbers.c
+++ Rank-02/Level-00/ft_print_numbers.c
@@ -10,13 +10,24 @@
 
 #include <unistd.h> // write
 
-int		main(void)
+void ft_print_numbers()
 {
-	char	number;
-
-	number = '0' - 1; // Or with a table (but I hate forcing!)
-	while (number++ != '9') // check fizzbuzz for example
+	char number;
+	
+	number = '0' - 1;
+	while (++number != '9' + 1)
 		write(1, &number, 1);
 	write(1, "\n", 1);
+}
+
+void ft_print_numbers_simple(void)
+{
+	write(1, "0123456789\n", 11);
+}
+
+int	main(void)
+{
+	ft_print_numbers_simple();
+	ft_print_numbers();
 	return (0);
 }
