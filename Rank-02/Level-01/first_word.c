6337dac00ff805382f5ef26e1c258b16d34be2db
ava_kyoko <ava8kyoko@gmail.com>
2024-02-02 14:42:10 -0500

    Update first_word.c

--- Rank-02/Level-01/first_word.c
+++ Rank-02/Level-01/first_word.c
@@ -26,18 +26,26 @@
 // lorem,ipsum$
 // $>
 
-#include <unistd.h> // write
+#include <unistd.h>
 
-int	main(int argc, char *argv[])
+int is_character(char character)
 {
-	int i;
+	if ((character >= 33 && character <= 126))
+		return (1);
+	else
+		return (0);
+}
 
+int	main(int argc, char **argv)
+{
+	int	i;
+	
 	i = 0;
-	if (argc > 1)
+	if (argc == 2)
 	{
 		while (argv[1][i] == ' ' || argv[1][i] == '\t')
 			i++;
-		while (argv[1][i] != ' ' && argv[1][i] != '\t' && argv[1][i] != '\0')
+		while (is_character(argv[1][i]) == 1)
 			write(1, &argv[1][i++], 1);
 	}
 	write(1, "\n", 1);
