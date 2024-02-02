c3ae095775e71b06523bb1edadf696ab59c20749
ava_kyoko <ava8kyoko@gmail.com>
2024-02-02 13:35:40 -0500

    Update maff_revalpha.c

--- Rank-02/Level-00/maff_revalpha.c
+++ Rank-02/Level-00/maff_revalpha.c
@@ -10,60 +10,21 @@
 // $> ./maff_revalpha | cat -e
 // zYxWvUtSrQpOnMlKjIhGfEdCbA$
 
-#include <unistd.h> // write
+#include <unistd.h>
 
-int		main(void)
+int	main(void)
 {
-	char		alpha;
+	char alpha;
 
-	alpha = 'z' + 1; // +1 because I decrement inside conditions.
-	while (alpha-- != 'A' + 32) // + 32 because it does an extra while
-	{							//  due to where write is. (Can force it, and place a write before the loop!)
+	alpha = 'z' + 1;
+	while (--alpha != 'a' - 1)
+	{
 		write(1, &alpha, 1);
-		if (alpha % 2) // alpha % 2 != 0 -> odd
-			alpha += 32; // -> change to uppercase
-		else  // alpha % 2 == 0 -> even
-			alpha -= 32; // -> change to lowercase
+		if (alpha % 2)
+			alpha += 32;
+		else
+			alpha -=32;
 	}
 	write(1, "\n", 1);
 	return (0);
 }
-
-// NOTE : Okay, there is a difference in the algorithm between maff_alpha and maff_revalalpha
-// because I use the ascii value (decimal) instead of the order position. man ASCII <3
-// A is 65, a is 97
-// Z is 90, z is 122
-
-// Another way to do that is to use another variable, i.e. an index :)
-// example :
-// int		main(void)
-// {
-// 	char		alpha;
-// 	int			i;
-	
-// 	write(1, "z", 1);
-// 	alpha = 'z';
-// 	i = 0;
-// 	while (++i != 26 && --alpha)
-// 	{
-// 		if (alpha % 2)
-// 			alpha -= 32;
-// 		else
-// 			alpha += 32;
-// 		write(1, &alpha, 1);
-// 	}
-// 	write(1, "\n", 1);
-// 	return (0);
-// }
-
-// You can use two tables (strings) too.
-// like that :
-// char upper[13] = "YWUSQOMKIGECA";
-// char lower[13] = "zxvtrpnljhfdb";	in a while with two conditions like upper examples.
-//
-// or just -> char reverse[26] = "zYxWvUtSrQpOnMlKjIhGfEdCbA"; in a while
-//
-// see fizzbuzz
-
-// Or be really lazy and do that... -> write(1, "zYxWvUtSrQpOnMlKjIhGfEdCbA", 26);
-// But I don't think this is a good way to learn better practices and understand concepts.
