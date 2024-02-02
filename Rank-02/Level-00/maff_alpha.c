186fb8a7dc2a156065ec8024ec1a8249199ccc81
ava_kyoko <ava8kyoko@gmail.com>
2024-02-02 13:28:12 -0500

    Update maff_alpha.c

--- Rank-02/Level-00/maff_alpha.c
+++ Rank-02/Level-00/maff_alpha.c
@@ -15,21 +15,29 @@
 
 #include <unistd.h> // write
 
-int		main(void)
+int	main(void)
 {
-	char		alpha;
+	char alpha;
 
-	alpha = 'a' - 1; // -1 because I increment inside conditions.
-	while (alpha++ != 'Z' + 32) // + 32 because it does an extra while
-	{							//  due to where write is. (Can force it, and place a write before the loop!)
-		write(1, &alpha, 1);
-		if (alpha % 2) // alpha % 2 != 0 -> odd
-			alpha -= 32; // change to lowercase
-		else  // alpha % 2 == 0 -> even
-			alpha += 32; // -> change to uppercase
+	alpha = 'a' - 1;
+	while (++alpha != 'z' + 1)
+	{
+		write(1, &alpha, 2);
+		if (alpha % 2) // odd
+			alpha -= 32;
+		else // even
+			alpha += 32;
 	}
 	write(1, "\n", 1);
 	return (0);
 }
 
-// NOTA BENE : see maff_alpha for other examples
+    //   64  @    65  A    66  B    67  C    68  D    69  E    70  F    71  G
+    //   72  H    73  I    74  J    75  K    76  L    77  M    78  N    79  O
+    //   80  P    81  Q    82  R    83  S    84  T    85  U    86  V    87  W
+    //   88  X    89  Y    90  Z    91  [    
+
+	//  96  `    97  a    98  b    99  c   100  d   101  e   102  f   103  g
+    //  104  h   105  i   106  j   107  k   108  l   109  m   110  n   111  o
+    //  112  p   113  q   114  r   115  s   116  t   117  u   118  v   119  w
+    //  120  x   121  y   122  z   123  { 
