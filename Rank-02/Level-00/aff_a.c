eba770c7d12a53bb59711320eb4a27d007ebc70c
ava_kyoko <ava8kyoko@gmail.com>
2024-03-30 15:18:57 -0400

    Update aff_a.c

--- Rank-02/Level-00/aff_a.c
+++ Rank-02/Level-00/aff_a.c
@@ -7,6 +7,29 @@
 // encounters in it, followed by a newline. If there are no 'a' characters in the
 // string, the program just writes a newline. If the number of parameters is not
 // 1, the program displays 'a' followed by a newline.
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    int i;
+    if (argc == 2)
+    {
+        i = -1;
+        while (argv[1][++i])
+        {
+            if (argv[1][i] == 'a')
+            {
+                write(1,&argv[1][i], 1);
+                break;
+            }
+        } 
+    }
+    else
+        write(1, "a", 1);
+    write(1, "\n", 1);
+    return (0);
+}
+
 
 // Example:
 
